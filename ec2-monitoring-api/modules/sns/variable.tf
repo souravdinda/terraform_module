1032c6dc8706e16f61d8eea1323190772f3a785e
SOURAV DINDA <63599752+souravdinda@users.noreply.github.com>
2021-12-05 19:37:47 +0530

    Add files via upload

---
+++ ec2-monitoring-api/modules/sns/variable.tf
@@ -0,0 +1,60 @@
+variable "TagOrgKey" {
+
+
+}
+variable "TagOrgValue" {
+
+  
+}
+variable "TagDepartmentKey" {
+  
+ 
+}
+variable "TagDepartmentValue" {
+
+
+}
+variable "TagTeamKey" {
+
+}
+variable "TagTeamValue" {
+
+  
+}
+variable "TagCostCenterKey" {
+
+}
+variable "TagCostCenterValue" {
+
+}
+variable "TagDataClassificationKey" {
+
+}
+variable "TagDataClassificationValue" {
+
+}
+variable "Namespace" {
+
+}
+variable "TopicEmail" {
+
+}
+
+variable "TagProjectKey" {
+
+  
+}
+variable "TagProjectValue" {
+ 
+  
+}
+variable "TagStageKey" {
+ 
+  
+}
+variable "TagStageValue" {
+
+  
+}
+
+
